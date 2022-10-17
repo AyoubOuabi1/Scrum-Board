2e4053cc7cd1fa58aabb32a9e7d3776733cba700
AyoubOuabi1 <ayoub.ouabi06@gmail.com>
2022-10-17 15:32:49 +0000

    add saveTask methode

--- starter/assets/js/js.js
+++ starter/assets/js/js.js
@@ -1,146 +1,178 @@
-var date = new Date();
- let newtasks=[
-    {
-        title:"Keep all the updated requirements in one place",
-        date: "#1 created in "+date.getFullYear()+"-"+(date.getMonth()+1)+"-"+ date.getDate(),
-        description : `There is hardly anything more frustrating than
-        having to look for current requirements in tens of comments under
-        the actual description or having to decide which commenter is
-        actually authorized to change the requirements. The goal here is
-        to keep all the up-to-date requirements and details in the
-        main/primary description of a task. Even though the information in
-        comments may affect initial criteria, just update this primary
-        description accordingly.`,
-        type: "feature",
-        priority:"low",
-        taskStatus:"TO DO"
-    },{
-        title:"Keep all the updated requirements in one place",
-        date: "#1 created in "+date.getFullYear()+"-"+(date.getMonth()+1)+"-"+ date.getDate(),
-        description : `There is hardly anything more frustrating than
-        having to look for current requirements in tens of comments under
-        the actual description or having to decide which commenter is
-        actually authorized to change the requirements. The goal here is
-        to keep all the up-to-date requirements and details in the
-        main/primary description of a task. Even though the information in
-        comments may affect initial criteria, just update this primary
-        description accordingly.`,
-        type: "feature",
-        priority:"low",
-        taskStatus:"PROGRESS"
+// SCRIPT WHERE WE NEED THE ARRAY
+//general variable section 
+let taskSection = document.getElementById("to-do-tasks");
+let progressSection = document.getElementById("in-progress-tasks");
+let doneSection = document.getElementById("done-tasks");
+let title=document.getElementById("title");
+let type=document.getElementsByName("type");
+let priority =document.getElementById("priority");
+let taskStatus=document.getElementById("status")
+let date =document.getElementById("date");
+let description=document.getElementById("Description");
+let modalTask=document.getElementById("modal-task");
 
-    },{
-        title:"Keep all the updated requirements in one place",
-        date: "#1 created in "+date.getFullYear()+"-"+(date.getMonth()+1)+"-"+ date.getDate(),
-        description : `There is hardly anything more frustrating than
-        having to look for current requirements in tens of comments under
-        the actual description or having to decide which commenter is
-        actually authorized to change the requirements. The goal here is
-        to keep all the up-to-date requirements and details in the
-        main/primary description of a task. Even though the information in
-        comments may affect initial criteria, just update this primary
-        description accordingly.`,
-        type: "feature",
-        priority:"low",
-        taskStatus:"TO DO"
+loadTables();
+function loadTables() {
+    var c;
+  for (var i = 0; i < tasksTables.length; i++) {
+    c=i+1;
+        
+    if (tasksTables[i].status == "To Do") {
+      taskSection.innerHTML+=`<button class="col-12 text-start btn-light text-black border-end-0 shadow-none">
+      <div class="row mt-2">
+        <div class="col-1 ">
+          <i class=" bi bi-exclamation-octagon text-red h1"></i>
+        </div>
+        <div class="col-11 ">
+          <div class="h3">${tasksTables[i].title}</div>
+          <div class="">
+            <div class="">#${c} created in ${tasksTables[i].date}</div>
+            <div class="" title="${tasksTables[i].description}">${tasksTables[i].description.substring(0,70)}</div>
+          </div>
+          <div class="mb-3 mt-2">
+            <span class="btn btn-primary ">${tasksTables[i].priority}</span>
+            <span class="btn btn-secondary ">${tasksTables[i].type}</span>
+          </div>
+        </div>
+      </div>
 
-    },{
-        title:"Keep all the updated requirements in one place",
-        date: "#1 created in "+date.getFullYear()+"-"+(date.getMonth()+1)+"-"+ date.getDate(),
-        description : `There is hardly anything more frustrating than
-        having to look for current requirements in tens of comments under
-        the actual description or having to decide which commenter is
-        actually authorized to change the requirements. The goal here is
-        to keep all the up-to-date requirements and details in the
-        main/primary description of a task. Even though the information in
-        comments may affect initial criteria, just update this primary
-        description accordingly.`,
-        type: "feature",
-        priority:"low",
-        taskStatus:"TO DO"
+    </button>`
+    } else if (tasksTables[i].status == "In Progress") {
+      progressSection.innerHTML+=`<button class="col-12 text-start btn-light text-black border-end-0 shadow-none">
+      <div class="row mt-2">
+        <div class="col-1 ">
+          <i class=" bi bi-arrow-clockwise text-green h1"></i>
+        </div>
+        <div class="col-11 ">
+          <div class="h3">${tasksTables[i].title}</div>
+          <div class="">
+            <div class="">#${c} created in ${tasksTables[i].date}</div>
+            <div class="" title="${tasksTables[i].description}">${tasksTables[i].description.substring(0,70)}</div>
+          </div>
+          <div class="mb-3 mt-2">
+            <span class="btn btn-primary ">${tasksTables[i].priority}</span>
+            <span class="btn btn-secondary ">${tasksTables[i].type}</span>
+          </div>
+        </div>
+      </div>
 
-    },{
-        title:"Keep all the updated requirements in one place",
-        date: "#1 created in "+date.getFullYear()+"-"+(date.getMonth()+1)+"-"+ date.getDate(),
-        description : `There is hardly anything more frustrating than
-        having to look for current requirements in tens of comments under
-        the actual description or having to decide which commenter is
-        actually authorized to change the requirements. The goal here is
-        to keep all the up-to-date requirements and details in the
-        main/primary description of a task. Even though the information in
-        comments may affect initial criteria, just update this primary
-        description accordingly.`,
-        type: "feature",
-        priority:"low",
-        taskStatus:"DONE"
+    </button>`
+    } else if (tasksTables[i].status == "Done")  {
+      doneSection.innerHTML+=`<button class="col-12 text-start btn-light text-black border-end-0 shadow-none">
+      <div class="row mt-2">
+        <div class="col-1 ">
+          <i class=" bi bi-check-circle text-green h1"></i>
+        </div>
+        <div class="col-11 ">
+          <div class="h3">${tasksTables[i].title}</div>
+          <div class="">
+            <div class="">#${c} created in ${tasksTables[i].date}</div>
+            <div class="" title="${tasksTables[i].description}">${tasksTables[i].description.substring(0,70)}</div>
+          </div>
+          <div class="mb-3 mt-2">
+            <span class="btn btn-primary ">${tasksTables[i].priority}</span>
+            <span class="btn btn-secondary ">${tasksTables[i].type}</span>
+          </div>
+        </div>
+      </div>
 
+    </button>`
+      
     }
- ]
- console.log(newtasks);
- 
-let taskSection=document.getElementById("to-do-tasks");
-let progressSection=document.getElementById("in-progress-tasks");
-let doneSection=document.getElementById("done-tasks");
+  }
+}
+/**
+ * In this file app.js you will find all CRUD functions name.
+ * 
+ */
 
+ function createTask() {
+    // initialiser task form
 
-function loadTables(){
-    let btn= document.createElement('button');
-    //add bootstrap classes into button 
-    btn.classList.add('col-12', 'text-start', 'btn-light', 'text-black', 'border-end-0', 'shadow-none');
-    let Div=document.createElement('div');
-    Div.classList.add('row', 'mt-2');
-    let iconDiv=document.createElement('div');
-    iconDiv.classList.add("col-1");
-    let icon=document.createElement('i');
-    let dataDiv=document.createElement('div');
-    dataDiv.classList.add("col-11");
-    let titleDiv=document.createElement('div');
-    titleDiv.classList.add("h3");
-    let infosDiv=document.createElement('div');
-    let dateDiv=document.createElement('div');
-    let desDiv=document.createElement('div');
-    let typeDiv=document.createElement('div');
-    typeDiv.classList.add('mb-3', 'mt-2');
-    let prioritySpan=document.createElement('span');
-    prioritySpan.classList.add('btn', 'btn-primary')
-    let typeSpan=document.createElement('span');
-    typeDiv.classList.add('btn', 'btn-secondary');
-    for(var i=0;i<newtasks.length;i++){
-       btn.appendChild(Div);
-       Div.appendChild(iconDiv);
-       iconDiv.appendChild(icon);
+    // Afficher le boutton save
     
-       Div.appendChild(dataDiv);
-       dataDiv.appendChild(titleDiv);
-       dataDiv.appendChild(infosDiv);
-       infosDiv.appendChild(dateDiv);
-       infosDiv.appendChild(desDiv);
-       dataDiv.appendChild(typeDiv);
-       typeDiv.appendChild(prioritySpan);
-       typeDiv.appendChild(typeSpan);
-        console.log(newtasks[i].title)
-       titleDiv.innerHTML=newtasks[i].title;
-       dataDiv.innerHTML=newtasks[i].date;
-       desDiv.innerHTML=newtasks[i].description.substring(0,70)+"...";
-       prioritySpan.innerHTML=newtasks[i].priority;
-       typeSpan.innerHTML=newtasks[i].type;
-       if(newtasks[i].taskStatus=="TO DO"){
-            taskSection.appendChild(btn);
-            icon.classList.add('bi', 'bi-exclamation-octagon', 'text-red', 'h1');
-
-       }else if(newtasks[i].taskStatus=="PROGRESS"){
-            progressSection.appendChild(btn);
-            icon.classList.add('bi', 'bi-arrow-clockwise', 'text-green', 'h1');
-
-       }else {
-            doneSection.appendChild(btn);
-            icon.classList.add('bi', 'bi-check-circle', 'text-green', 'h1');
-
-       }
-
-    }
+    // Ouvrir modal form
 }
 
- function AddTask(){
-    alert("work");
- }
+function saveTask() {
+    // Recuperer task attributes a partir les champs input
+
+    // Créez task object
+
+    // Ajoutez object au Array
+
+    // refresh tasks
+    taskSection.innerHTML='';
+    progressSection.innerHTML='';
+    doneSection.innerHTML='';
+    let typeChecked;
+    for(let i=0;i<type.length;i++){
+      if(type[i].checked){
+        typeChecked=type[i].value;
+      }
+    }
+    let taskObject={
+      'title'         :   title.value,
+      'type'          :   typeChecked,
+      'priority'      :   priority.value,
+      'status'        :   taskStatus.value,
+      'date'          :   date.value,
+      'description'   :   description.value,
+  
+    }
+    tasksTables[tasksTables.length]=taskObject;
+     
+    loadTables();
+
+    
+}
+
+function editTask(index) {
+    // Initialisez task form
+
+    // Affichez updates
+
+    // Delete Button
+
+    // Définir l’index en entrée cachée pour l’utiliser en Update et Delete
+
+    // Definir FORM INPUTS
+
+    // Ouvrir Modal form
+}
+
+function updateTask() {
+    // GET TASK ATTRIBUTES FROM INPUTS
+
+    // Créez task object
+
+    // Remplacer ancienne task par nouvelle task
+
+    // Fermer Modal form
+
+    // Refresh tasks
+    
+}
+
+function deleteTask() {
+    // Get index of task in the array
+
+    // Remove task from array by index splice function
+
+    // close modal form
+
+    // refresh tasks
+}
+
+function initTaskForm() {
+    // Clear task form from data
+
+    // Hide all action buttons
+}
+
+function reloadTasks() {
+    // Remove tasks elements
+
+    // Set Task count
+}
+
